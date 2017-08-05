2c1b94e063969195215b73aa2ba30afef40e3c6c
Alex Pessoa <alexpessoajr@gmail.com>
2017-08-05 01:15:50 -0300

    Fills the board dynamically

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
 import './Board.css';
 import Card from '../card/Card';
+import jsLogo from '../../assets/javascript.png';
+
+const range = (size, content) => Array(size).fill(content);
 
 export default class Board extends Component {
+
+  constructor(props) {
+    super(props);
+    let cardsMatrix = range(4).map(row => 
+      range(4, {
+        logo: jsLogo,
+        name: 'Javascript'
+      })
+    );
+    this.state = { cardsMatrix };
+  }
+
   render() {
     return (
       <div className="board">
-        <div>
-          <Card />
-          <Card />
-          <Card />
-        </div>
-        <div>
-          <Card />
-          <Card />
-          <Card />
-        </div>
-        <div>
-          <Card />
-          <Card />
-          <Card />
-        </div>
+        {this.state.cardsMatrix.map((cardsRow, rowIdx) =>
+          <div className="row" key={`row-${rowIdx}`}>
+            {cardsRow.map((card, cardIdx) =>
+              <Card image={card.logo} name={card.name} key={`card-${cardIdx}`} />
+            )}
+          </div>
+        )}
       </div>
     );
   }
