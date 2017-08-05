ebc218937239aa776dd821d15e189ca1f99345cf
Alex Pessoa <alexpessoajr@gmail.com>
2017-08-05 11:43:54 -0300

    Aligns the board to the center of the screen

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Board from './components/board/Board';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <div className="App-intro">
-          <Board />
-        </div>
+      <div>
+        <Board />
       </div>
     );
   }
